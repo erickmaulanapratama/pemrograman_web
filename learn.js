b5d48757d3744a7aee576ddeae0f8562bae35df5
Erick Maulana Pratama <erickmaulanapratama@gmail.com>
2022-01-04 00:26:13 +0700

    Latihan 5.3: Melakukan operasi sederhana dengan operator perbandingan

--- learn.js
+++ learn.js
@@ -3,11 +3,24 @@ let myString = "Hello";
 let myBoolean = true;
 const phi = 3.14;
 
-console.log = myNumber;
-console.log = myString;
-console.log = myBoolean;
-console.log = phi;
+console.log (myNumber);
+console.log (myString);
+console.log (myBoolean);
+console.log (phi);
 
 window.alert ("Angka favorit saya :" + myNumber);
 window.alert ('Angka favorit saya adalah : ' + myNumber);
 window.alert (`Angka favorit saya adalah : ${myNumber}`);
+
+let x=8;
+let y=4;
+console.log(x, y); // menampilkan 8, 4
+console.log('x + y =', x + y); // menampilkan 12
+console.log('x - y =', x - y); // menampilkan 4
+console.log('x * y =', x * y); // menampilkan 32
+console.log('x / y =', x / y); // menampilkan 2
+console.log('x % y =', x % y); // menampilkan 0
+console.log('x++ =', x++); // menampilkan 8
+console.log('++x =', ++x); // menampilkan 10
+console.log('x-- =', x--); // menampilkan 10
+console.log('--x =', --x); // menampilkan 8
