75e22a396ef8c71e1446c53e9be211128982b3a2
Erick Maulana Pratama <erickmaulanapratama@gmail.com>
2022-01-04 00:33:56 +0700

    Latihan 5.2: Melakukan operasi sederhana dengan operator aritmatika*

--- learn.js
+++ learn.js
@@ -3,17 +3,17 @@ let myString = "Hello";
 let myBoolean = true;
 const phi = 3.14;
 
-console.log (myNumber);
-console.log (myString);
-console.log (myBoolean);
-console.log (phi);
+console.log(myNumber);
+console.log(myString);
+console.log(myBoolean);
+console.log(phi);
 
-window.alert ("Angka favorit saya :" + myNumber);
-window.alert ('Angka favorit saya adalah : ' + myNumber);
-window.alert (`Angka favorit saya adalah : ${myNumber}`);
+window.alert("Angka favorit saya :" + myNumber);
+window.alert('Angka favorit saya adalah : ' + myNumber);
+window.alert(`Angka favorit saya adalah : ${myNumber}`);
 
-let x=8;
-let y=4;
+let x = 8;
+let y = 4;
 console.log(x, y); // menampilkan 8, 4
 console.log('x + y =', x + y); // menampilkan 12
 console.log('x - y =', x - y); // menampilkan 4
@@ -24,3 +24,15 @@ console.log('x++ =', x++); // menampilkan 8
 console.log('++x =', ++x); // menampilkan 10
 console.log('x-- =', x--); // menampilkan 10
 console.log('--x =', --x); // menampilkan 8
+
+let a = 8;
+let b = 4;
+console.log(a, b); // menampilkan 8, 4
+console.log('a > b =', a > b); // menampilkan true
+console.log('a < b =', a < b); // menampilkan false
+console.log('a >= b =', a >= b); // menampilkan true
+console.log('a <= b =', a <= b); // menampilkan false
+console.log('a == b =', a == b); // menampilkan false
+console.log('a != b =', a != b); // menampilkan true
+console.log('a === b =', a === b); // menampilkan false
+console.log('a !== b =', a !== b); // menampilkan false
